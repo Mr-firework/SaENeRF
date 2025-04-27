00d0750ad265c8c827232a95ebdfba28b65dff66
Mr-firework <357797666@qq.com>
2025-04-27 21:14:17 +0800

    Update dataparser_config.py

--- saenerf/dataparser_config.py
+++ saenerf/dataparser_config.py
@@ -4,5 +4,3 @@ from saenerf.data.saenerf_dataparser import SaENeRFDataParserConfig
 from saenerf.data.eds_dataparser import EDSDataparserConfig
 
 saenerf_dataparser = DataParserSpecification(config=SaENeRFDataParserConfig())
-
-eds_dataparser = DataParserSpecification(config=EDSDataparserConfig())
